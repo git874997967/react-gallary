c99b40b7a0adb2bc0b8e98793fe9f111ffee2e1b
git874997967 <g874997967@gmail.com>
2017-12-10 21:26:11 -0800

    实现画廊应用 拖了一个学期  期末终于着手实现了

---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+/*let yeomanImage = require('../images/yeoman.png');
+* ../images/1.gif*/
+//let imageData=require('../data/imageData.json');
+ /*imageData.each(function(){
+   return this.fileName="../images"+this.fileName;
+ });*/
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+     <section className='stage'>
+       <section className='img-sec'>
+
+       </section>
+       <nav className='controller-nav'>
+cvbcvbcvb cvb
+       </nav>
+     </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
